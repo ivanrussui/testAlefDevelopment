8f0809be300ef6a9bdf62978ec05a9e415367964
Ivan <fenomen_ny@mail.ru>
2020-12-08 22:47:57 +0300

    description + js

--- src/js/btn.js
+++ src/js/btn.js
@@ -1,8 +1,15 @@
-document.getElementsByClassName('.cards__button').function	() {
+// document.getElementsByClassName('.cards__button').function	() {
 
-	const btn = document('.cards__button');
+// 	const btn = document('.cards__button');
 
-	btn.on('click', function () {
+// 	btn.on('click', function () {
 		
+// 	});
+// }
+
+// цвет меняется но во всех и обратно нет
+$(document).ready(function () {
+	$('.cards__button').click(function (event) {
+		$('.cards__button').css('background-color', 'black')
 	});
-}
+});
