ef1ce68ccdfbbaf69cbe52b885ecae136cdaf37c
Ivan <fenomen_ny@mail.ru>
2020-12-08 18:56:40 +0300

    phone

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,10 @@
 $(document).ready(function () {
   $('.navbar__burger').click(function (event) { 
 		$('.navbar__burger, .navbar__menu').toggleClass('active');
+		// $('.navbar__burger, .navbar__menu').toggleClass('active');
+		// $('.navbar__phone').html('display', 'none');
 		$('body').toggleClass('lock');
+
 	});
 });
 
